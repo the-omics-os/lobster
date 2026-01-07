43a8988aadb0fc86210a27f57ec24bf1b82893f1
Tyo <kevin.yar@outlook.com>
2026-01-08 00:47:22 +0100

    0.4.2.0

--- scripts/generate_allowlist.py
+++ scripts/generate_allowlist.py
@@ -262,6 +262,7 @@ lobster/config/constants.py
 lobster/config/base_config.py
 lobster/config/model_service.py
 lobster/config/ollama_service.py
+lobster/config/ssl_setup.py
 lobster/config/README_CONFIGURATION.md
 lobster/config/README_THINKING.md
 
